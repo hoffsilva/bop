fd231dd42bb2976250bb93c8bd90f65d799141bd
Hoff Silva <hoff.henry@gmail.com>
2018-02-17 16:56:53 -0200

    Added project; Added Podfile; Added library swiftnotice;

---
+++ Bop/Bop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bop
+//
+//  Created by Hoff Henry Pereira da Silva on 17/02/2018.
+//  Copyright © 2018 Hoff Henry Pereira da Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
