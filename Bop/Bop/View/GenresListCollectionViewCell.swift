54b825782448045ae44d2f73f04347ed21514c6f
Hoff Silva <hoff.henry@gmail.com>
2018-02-17 23:53:48 -0200

    Added ViewControllers; Added Movie model;

---
+++ Bop/Bop/View/GenresListCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  GenresListCollectionViewCell.swift
+//  Bop
+//
+//  Created by Hoff Henry Pereira da Silva on 17/02/2018.
+//  Copyright © 2018 Hoff Henry Pereira da Silva. All rights reserved.
+//
+
+import UIKit
+
+class GenresListCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var genreImageView: UIImageView!
+    
+}
---
+++ Bop/Bop/View/MoviesListTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  MoviesListTableViewCell.swift
+//  Bop
+//
+//  Created by Hoff Henry Pereira da Silva on 17/02/2018.
+//  Copyright © 2018 Hoff Henry Pereira da Silva. All rights reserved.
+//
+
+import UIKit
+
+class MoviesListTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    @IBOutlet weak var genreListsCollectionView: UICollectionView!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
